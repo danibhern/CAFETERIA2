5edeb3fae44ef45268701d6ab28600cf50af07f2
danibhern <dan.barrerah@duocuc.cl>
2025-09-05 19:28:54 -0400

    edicion en mensajes de error para reserva

--- functions_reserva.js
+++ functions_reserva.js
@@ -11,50 +11,50 @@ const set = new Set();
 nombre.addEventListener('keyup',function(e){
     if(nombre.value.length <=3){
         nombre.classList.add("error");
-        set.add("<p> Completar campo Nombre.</p>");
+        set.add("<p> Completar campo con nombre y apellido.</p>");
     }else{
         nombre.classList.remove("error");
-        set.delete("<p> Completar campo Nombre.</p>");
+        set.delete("<p> Completar campo con nombre y apellido.</p>");
     }
 });
 
 email.addEventListener('keyup',function(e){
     if(!email.value.includes("@")){
         email.classList.add("error");
-        set.add("<p> Completar campo Email con un @. </p>");
+        set.add("<p> El campo email debe contener @. </p>");
     }else{
         email.classList.remove("error");
-        set.delete("<p> Completar campo Email con un @. </p>");
+        set.delete("<p> El campo email debe contener @. </p>");
     }
 });
 
 telefono.addEventListener('keyup',function(e){
     if(telefono.value.length !==9){
         telefono.classList.add("error");
-        set.add("<p> Completar campo Teléfono con solo 9 dígitos. </p>");
+        set.add("<p> El campo teléfono debe contener 9 dígitos. </p>");
     }else{
         telefono.classList.remove("error");
-        set.delete("<p> Completar campo Teléfono con solo 9 dígitos. </p>");
+        set.delete("<p> El campo teléfono debe contener 9 dígitos. </p>");
     }
 });
 
 fecha.addEventListener('change',function(e){
     if(!fecha.value){
         fecha.classList.add("error");
-        set.add("<p> Completar campo Fecha. </p>");
+        set.add("<p> Debe seleccionar una fecha. </p>");
     } else {
         fecha.classList.remove("error");
-        set.delete("<p> Completar campo Fecha. </p>");
+        set.delete("<p> Debe seleccionar una fecha. </p>");
     }
 });
 
 hora.addEventListener('change',function(e){
     if(!hora.value){
         hora.classList.add("error");
-        set.add("<p> Completar campo Hora. </p>");
+        set.add("<p> Debe seleccionar una hora para su reserva. </p>");
     } else {
         hora.classList.remove("error");
-        set.delete("<p> Completar campo Hora. </p>");
+        set.delete("<p> Debe seleccionar una hora para su reserva. </p>");
     }
 });
 
